eb66034a030c0f941d35d74d31511e666c6d431d
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-11-08 02:02:07 -0500

    Updates

---
+++ precache-manifest.db302999795b1ffc39fed4762b73dfb1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309695cb1d49dd0cbfe37aa9585d3c48",
+    "url": "/MiguelMartheynRewardStore/index.html"
+  },
+  {
+    "revision": "35984668471a64043bcf",
+    "url": "/MiguelMartheynRewardStore/static/css/main.93f524e0.chunk.css"
+  },
+  {
+    "revision": "0825e6bea43c38d99fa1",
+    "url": "/MiguelMartheynRewardStore/static/js/2.05697972.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/MiguelMartheynRewardStore/static/js/2.05697972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35984668471a64043bcf",
+    "url": "/MiguelMartheynRewardStore/static/js/main.85c30419.chunk.js"
+  },
+  {
+    "revision": "3b857b8645f9c01e4249",
+    "url": "/MiguelMartheynRewardStore/static/js/runtime-main.a543daf2.js"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/MiguelMartheynRewardStore/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/MiguelMartheynRewardStore/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "49efb318471ecd7bcd27784dfa15e9fb",
+    "url": "/MiguelMartheynRewardStore/static/media/compras.49efb318.png"
+  },
+  {
+    "revision": "b8bda255dabb711910faeca9ccf6dfd9",
+    "url": "/MiguelMartheynRewardStore/static/media/cry.b8bda255.png"
+  },
+  {
+    "revision": "d016bdb25d332de78e049ff90410f2b6",
+    "url": "/MiguelMartheynRewardStore/static/media/happy.d016bdb2.png"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/MiguelMartheynRewardStore/static/media/main.f0b98264.png"
+  }
+]);
